14d9bbb4ef64e760cbd55433eb1e8cce6a1a5e01
Bryan Pearson <peabryan95@gmail.com>
2021-12-15 12:00:45 -0500

    Added all packet types to fuzzing engine

--- fume/fuzzing_engine.py
+++ fume/fuzzing_engine.py
@@ -1,4 +1,18 @@
 from generators.connect import Connect
+from generators.connack import Connack
+from generators.publish import Publish
+from generators.puback import Puback
+from generators.pubrec import Pubrec
+from generators.pubrel import Pubrel
+from generators.pubcomp import Pubcomp
+from generators.subscribe import Subscribe
+from generators.suback import Suback
+from generators.unsubscribe import Unsubscribe
+from generators.unsuback import Unsuback
+from generators.pingreq import Pingreq
+from generators.pingresp import Pingresp
+from generators.disconnect import Disconnect
+from generators.auth import Auth
 
 import helper_functions.print_verbosity as pv
 import globals as g
@@ -12,18 +26,22 @@ def corpus_to_array(file):
         lines[index] = line.replace("\n", "")
     return lines
 
-# Generate or seed a CONNECT packet
-def handle_state_connect(mm):
+def handle_select_or_generation_state(mm, packet):
+    state_name = mm.current_state.name
+
     if mm.model_type == 'mutation':
-        file = open("mqtt_corpus/CONNECT", "r")
+        file = open("mqtt_corpus/" + state_name, "r")
         lines = corpus_to_array(file)
         payload = random.choice(lines)
         g.payload.append(payload)
-        pv.debug_print("Payload so far: %s" % "".join(payload))
+        pv.debug_print("Added payload %s" % payload)
+        pv.debug_print("Payload so far: %s" % "".join(g.payload))
     else:
-        g.payload.append(Connect())
+        payload = packet()
+        g.payload.append(payload)
+        pv.debug_print("Added payload %s" % payload.toString())
         pv.debug_print("Payload so far: %s" % "".join([p.toString() for p in g.payload]))
-
+    
 # Handle the next state in the model
 def handle_state(mm):
     state = mm.current_state.name
@@ -32,8 +50,8 @@ def handle_state(mm):
     if state == 'S0':
         g.payload = []
 
-    # In state RESPONSE_LOG, we select a payload from the previous responses
-    # If the previous responses are empty, we just set the state to CONNECT
+    # In state RESPONSE_LOG, we select a payload from the previous responses. If the previous responses are empty, we just set 
+    # the state to CONNECT
     elif state == 'RESPONSE_LOG':
         if len(g.network_response_log) > 0:
             g.payload += random.choice(g.network_response_log)
@@ -41,8 +59,55 @@ def handle_state(mm):
             mm.current_state = mm.state_connect
             handle_state(mm)
 
+    # For the packet-specific states, we either connect or generate
+    # the desired packet and append it to the payload
+
     elif state == 'CONNECT':
-        handle_state_connect(mm)
+        handle_select_or_generation_state(mm, Connect)
+
+    elif state == 'CONNACK':
+        handle_select_or_generation_state(mm, Connack)
+
+    elif state == 'PUBLISH':
+        handle_select_or_generation_state(mm, Publish)
+
+    elif state == 'PUBACK':
+        handle_select_or_generation_state(mm, Puback)
+    
+    elif state == 'PUBREC':
+        handle_select_or_generation_state(mm, Pubrec)
+
+    elif state == 'PUBREL':
+        handle_select_or_generation_state(mm, Pubrel)
+
+    elif state == 'PUBCOMP':
+        handle_select_or_generation_state(mm, Pubcomp)
+
+    elif state == 'SUBSCRIBE':
+        handle_select_or_generation_state(mm, Subscribe)
+
+    elif state == 'SUBACK':
+        handle_select_or_generation_state(mm, Suback)
+
+    elif state == 'UNSUBSCRIBE':
+        handle_select_or_generation_state(mm, Unsubscribe)
+
+    elif state == 'UNSUBACK':
+        handle_select_or_generation_state(mm, Unsuback)
+
+    elif state == 'PINGREQ':
+        handle_select_or_generation_state(mm, Pingreq)
+    
+    elif state == 'PINGRESP':
+        handle_select_or_generation_state(mm, Pingresp)
+
+    elif state == 'DISCONNECT':
+        handle_select_or_generation_state(mm, Disconnect)
+
+    elif state == 'AUTH':
+        handle_select_or_generation_state(mm, Auth)
+
+
 
 # Run the fuzzing engine (indefinitely)
 # mm: the markov model
