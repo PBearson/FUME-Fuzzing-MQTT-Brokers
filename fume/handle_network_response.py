4137918cc639f2847a5a98c1cf15d665a793bd3d
Bryan Pearson <peabryan95@gmail.com>
2021-12-16 12:49:00 -0500

    Engine now logs unique G fields and grabs the corresponding requests

--- fume/handle_network_response.py
+++ fume/handle_network_response.py
@@ -1,23 +1,26 @@
 from parsers.parse_initializer import ParseInitializer
 import helper_functions.determine_protocol_version as hpv
+import helper_functions.print_verbosity as pv
 import globals as g
 
+# Handle the network response -- log the request if 
+# the response was unique.
+# A response is unique if its G field has never been seen before.
 def handle_network_response(recv):
-    print("Handling response: %s" % recv.hex())
     if len(recv) == 0:
         return
 
     if g.protocol_version == 0:
         g.protocol_version = hpv.determine_protocol_version(recv.hex())
-    
-    print("Using protocol version %d" % g.protocol_version)
-    
+        
     index = 0
     while index < len(recv.hex()):
-
-        print("Handling payload: %s" % recv.hex()[index:])
         parser = ParseInitializer(recv.hex()[index:], g.protocol_version)
-        
-        print(parser.parser.G_fields)
-        print(parser.parser.H_fields)
+
+        # Log G fields
+        G_fields = str(parser.parser.G_fields)
+        if G_fields not in g.network_response_log.keys():
+            g.network_response_log[G_fields] = g.payload
+            pv.normal_print("Found new response (%d found)" % len(g.network_response_log.keys()))
+
         index +=  2 * (parser.parser.remainingLengthToInteger()) + 2 + len(parser.parser.remaining_length)
