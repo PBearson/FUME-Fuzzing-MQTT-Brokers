93e7593f82739676d1e781ac74de53f20d044d11
Bryan Pearson <peabryan95@gmail.com>
2021-12-21 18:00:20 -0500

    Added console response

--- fume/handle_console_response.py
+++ fume/handle_console_response.py
@@ -1,5 +1,25 @@
+import globals as g
+from difflib import SequenceMatcher
+import helper_functions.print_verbosity as pv
+
+def check_similarity(line):
+    for response in g.console_response_log:
+        similarity = SequenceMatcher(None, line, response).ratio()
+        
+        # If there is another response that is similar enough, then
+        # we don't want the new response
+        if similarity >= g.SIMILARITY_THRESHOLD:
+            return True
+    return False
+
 def handle_console_response(proc):
     for line in iter(proc.stdout.readline, b''):
         # TODO add similarity threshold to config file
         # TODO log line if it is unique enough
-        pass
+        if len(line) == 0:
+            continue
+        if line not in g.console_response_log:
+            similarity = check_similarity(line)
+            if similarity is False and type(g.payload) is bytearray:
+                g.console_response_log[line] = g.payload
+                pv.normal_print("Found new console response (%d found)" % len(g.console_response_log.keys()))
